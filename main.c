bca26f40c53965bcb76735eff693480d2faa588f
MalikL2005 <163858025+MalikL2005@users.noreply.github.com>
2024-11-06 10:35:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -46,30 +46,80 @@ int compare(char s1[], char s2[]){
     return strcmp (s1, s2) == 0;
 }
 
-int handle_command(InputBuffer* buffer){
+
+int handle_command(InputBuffer* buffer, database *current_db, database *DBS[]){
     if (compare(buffer->buffer, "SHOWCOLUMNS")){
         printf("Showing columns...\n");
+        printf("%s\n", current_db->name);
         //show_columns(buffer);
-    } else if (compare(buffer->buffer, "EXIT")){
+    } 
+    else if (compare(buffer->buffer, "EXIT")){
         printf("Exiting... \n");
         return 1;
-    } else if (compare(buffer->buffer, "")){
+    } 
+    else if (compare(buffer->buffer, "USEDATABASE")){
+        int index; //not memory efficient
+        printf("Give db index\n");
+        scanf("%d", &index); // Todo: validate input
+        if (index < 5 && index >= 0 && DBS[index]){
+            current_db = DBS[index];
+        } else {
+            printf("This index is not valid.\n");
+        }
+    } 
+    else if (compare(buffer->buffer, "CREATEDATABASE")){ // Todo: add regex to check for "COMMAND <space> NAME"
+        // Read DB name
+        char dbname[MAX_DB_NAME_LENGTH]; //not memory efficient
+        printf("Give db name: ");
+        scanf("%s", dbname); // Todo: validate input 
+        
+        // Check if DB already exists
+        for (int i=0; i<MAX_DATABASES; i++){
+            if (!DBS[i]){
+                current_db = create_db(dbname);
+                DBS[i] = current_db;
+                printf("DB %s created.\n", dbname);
+                return 0;
+            }
+            if (compare(dbname, DBS[i]->name)){
+                printf("DB already exists\n");
+                continue; 
+            }
+        }
+        
+
+        
+    }
+    else if (compare(buffer->buffer, "SHOWDATABASES")){
+        printf("| Your DBs\n|-----------\n");
+        for (int i=0; i<MAX_DATABASES; i++){
+            if (DBS[i]){
+                printf("|---- %s\n", DBS[i]->name);
+            } else {
+                printf("|---- Empty slot\n");
+            }
+        }
+    } 
+    else if (compare(buffer->buffer, "")){
         return 0;
-    } else {
+    } 
+    else {
         printf("Unknown command.\n");
     }
     return 0;
 }
 
 
+
 int main() {
+    database *DBS [MAX_DATABASES];
     InputBuffer* pBuffer = newInputBuffer();
     int end;
-    
+    database *current_db;
     do {
         print_prompt();
         read_input(pBuffer);
-        end = handle_command(pBuffer);
+        end = handle_command(pBuffer, current_db, DBS);
     } while(end != 1);
     
     return 0;
